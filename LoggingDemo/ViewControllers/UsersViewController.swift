0ae973f07144a587e597b8b1dbf70377a2ab12a9
Al Wold <alwold@alwold.com>
2017-08-28 21:56:43 -0700

    add some fake stuff to add a user

--- LoggingDemo/ViewControllers/UsersViewController.swift
+++ LoggingDemo/ViewControllers/UsersViewController.swift
@@ -23,4 +23,64 @@ class UsersViewController: UITableViewController {
         cell.textLabel?.text = users[indexPath.row].name
         return cell
     }
+    
+    @IBAction func addButtonTapped(_ sender: Any) {
+        let alert = UIAlertController(title: "Add User", message: "Please enter a name for the user", preferredStyle: .alert)
+        alert.addTextField()
+        alert.addAction(
+            UIAlertAction(title: "Add", style: .default, handler:
+                { action in
+                    if let name = alert.textFields?.first?.text {
+                        self.addUser(name: name)
+                    } else {
+                        os_log("User name not entered", log: Logs.shared.defaultLog, type: .info)
+                        // show error
+                    }
+                }
+            )
+        )
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(alert, animated: false)
+    }
+    
+    func addUser(name: String) {
+        os_log("Adding user: %@", log: Logs.shared.defaultLog, type: .debug, name)
+        DispatchQueue.global(qos: .background).async {
+            self.users.append(User(name: name))
+            DispatchQueue.main.async {
+                os_log("Triggering tableview refresh", log: Logs.shared.defaultLog, type: .debug)
+                self.tableView.reloadData()
+            }
+        }
+        DispatchQueue.global(qos: .background).async {
+            self.randomSleep()
+            os_log("Queueing user to be added to some data store", log: Logs.shared.defaultLog, type: .debug)
+            self.randomSleep()
+            os_log("Adding to data store", log: Logs.shared.defaultLog, type: .debug)
+            self.randomSleep()
+            os_log("Writing thingamabobs to filesystem", log: Logs.shared.defaultLog, type: .debug)
+            self.randomSleep()
+            os_log("Syncing write", log: Logs.shared.defaultLog, type: .debug)
+        }
+        DispatchQueue.global(qos: .background).async {
+            self.randomSleep()
+            os_log("Sending user to network service", log: Logs.shared.defaultLog, type: .debug)
+            self.randomSleep()
+            os_log("Looking up DNS entry", log: Logs.shared.defaultLog, type: .debug)
+            self.randomSleep()
+            os_log("Connecting to server", log: Logs.shared.defaultLog, type: .debug)
+            self.randomSleep()
+            os_log("negotiating TLS", log: Logs.shared.defaultLog, type: .debug)
+            self.randomSleep()
+            os_log("Writing bytes", log: Logs.shared.defaultLog, type: .debug)
+            self.randomSleep()
+            os_log("Reading bytes", log: Logs.shared.defaultLog, type: .debug)
+            self.randomSleep()
+            os_log("Received 200 status, yay!", log: Logs.shared.defaultLog, type: .debug)
+        }
+    }
+    
+    func randomSleep() {
+        usleep(arc4random_uniform(UInt32(USEC_PER_SEC/3)))
+    }
 }
